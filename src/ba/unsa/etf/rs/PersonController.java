9060a448c0af85c7df09aa8aff089595330e08c7
Elma <etrako1@etf.unsa.ba>
2021-07-06 17:03:27 +0200

    dodan izgled prozora za prikaz studenata, povezano

--- src/ba/unsa/etf/rs/PersonController.java
+++ src/ba/unsa/etf/rs/PersonController.java
@@ -1,4 +1,79 @@
 package ba.unsa.etf.rs;
 
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.time.LocalDate;
+
 public class PersonController {
+
+    @FXML
+    private TableView tablePerson;
+    @FXML
+    private TableColumn colName, colNumberPoints, colDate;
+    @FXML
+    private PersonDAO model;
+    Stage editPerson = new Stage();
+    Stage aboutWindow = new Stage();
+    @FXML
+    private Label statusMsg;
+
+    public PersonController(PersonDAO model) {
+        this.model = model;
+    }
+
+    @FXML
+    private void updateSelectedPerson() {
+        Person p = (Person) tablePerson.getSelectionModel().getSelectedItem();
+        //model.setCurrentPerson(p);
+    }
+
+    @FXML
+    public void initialize() {
+        statusMsg.setText("Program started.");
+        colName.setCellValueFactory(new PropertyValueFactory<Person, String>("name"));
+        colNumberPoints.setCellValueFactory(new PropertyValueFactory<Person, Integer>("numberPoints"));
+        colDate.setCellValueFactory(new PropertyValueFactory<LocalDate, String>("date"));
+        //tablePerson.setItems(model.getPersons());
+        tablePerson.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Person>() {
+            @Override
+            public void changed(ObservableValue<? extends Person> observableValue, Person oldPerson, Person newPerson) {
+                if (oldPerson != null) {
+                }
+                if (oldPerson == null) {
+
+                } else {
+                    updateSelectedPerson();
+                }
+                tablePerson.refresh();
+            }
+        });
+    }
+
+
+    public void exit(ActionEvent actionEvent) {
+        Platform.exit();
+        System.exit(0);
+    }
+
+    public void addPerson(ActionEvent actionEvent) {
+    }
+
+    public void editPerson(ActionEvent actionEvent) {
+    }
+
+    public void deletePerson(ActionEvent actionEvent) {
+    }
+
+    public void openAbout(ActionEvent actionEvent) {
+    }
 }
