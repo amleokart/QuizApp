d2cc7009e68f6bf26770dd279b689e650fa9a8c2
Elma <etrako1@etf.unsa.ba>
2021-07-09 18:07:41 +0200

    izmijenjena forma za login

--- src/ba/unsa/etf/rs/LoginController.java
+++ src/ba/unsa/etf/rs/LoginController.java
@@ -7,35 +7,57 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
-import javax.swing.*;
 import java.io.IOException;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class LoginController {
 
-    public TextField usernameFld;
-    public PasswordField passwordFld;
-    public Button loginBtn;
+    public TextField fldName;
+    public TextField fldUsername;
+    public PasswordField fldPassword;
+    public Button btnLogin;
 
     @FXML
     public void initialize() {
-        usernameFld.getStyleClass().add("fieldIncorrect");
+        fldName.getStyleClass().add("fieldIncorrect");
+        fldUsername.getStyleClass().add("fieldIncorrect");
+        fldPassword.getStyleClass().add("fieldIncorrect");
 
-        usernameFld.textProperty().addListener(new ChangeListener<String>() {
+        fldName.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
                 if (n.isEmpty()) {
-                    usernameFld.getStyleClass().add("fieldIncorrect");
+                    fldName.getStyleClass().add("fieldIncorrect");
                 } else {
-                    usernameFld.getStyleClass().removeAll("fieldIncorrect");
+                    fldName.getStyleClass().removeAll("fieldIncorrect");
+                }
+            }
+        });
+
+        fldUsername.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                if (n.isEmpty()) {
+                    fldUsername.getStyleClass().add("fieldIncorrect");
+                } else {
+                    fldUsername.getStyleClass().removeAll("fieldIncorrect");
+                }
+            }
+        });
+
+        fldPassword.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                if (n.isEmpty()) {
+                    fldPassword.getStyleClass().add("fieldIncorrect");
+                } else {
+                    fldPassword.getStyleClass().removeAll("fieldIncorrect");
                 }
             }
         });
@@ -43,31 +65,21 @@ public class LoginController {
 
     public void loginAction(ActionEvent actionEvent) {
         Stage stage = new Stage();
-        if (usernameFld.getText() == "Elma" && passwordFld.getText() == "Telma") {
-            try {
-                PersonDAO model = PersonDAO.getInstance();
-                Locale.setDefault(new Locale("en", "US"));
-                ResourceBundle bundle = ResourceBundle.getBundle("languages");
-                //FXMLLoader loader = new FXMLLoader();
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/PersonList.fxml"), bundle);
-                LoginController ctrl = new LoginController();
-                //FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/PersonList.fxml"));
-                loader.setController(new PersonController(model));
-                Parent root = loader.load();
-                stage.setTitle("List");
-                stage.setScene(new Scene(root, 600, 500));
-                stage.show();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-            Alert alert = new Alert(Alert.AlertType.WARNING);
-            alert.setTitle("Warning");
-            alert.setHeaderText("Greska!");
-            alert.showAndWait();
+        try {
+            PersonDAO model = PersonDAO.getInstance();
+            Locale.setDefault(new Locale("en", "US"));
+            ResourceBundle bundle = ResourceBundle.getBundle("languages");
+            //FXMLLoader loader = new FXMLLoader();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/PersonList.fxml"), bundle);
+            LoginController ctrl = new LoginController();
+            //FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/PersonList.fxml"));
+            loader.setController(new PersonController(model));
+            Parent root = loader.load();
+            stage.setTitle("List");
+            stage.setScene(new Scene(root, 600, 500));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    }
-
-    public void mouse(MouseEvent mouseEvent) {
     }
 }
